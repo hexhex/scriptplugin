9279ce1a73b94d902aa1319de9cbe97c04f9c361
Thomas Krennwallner <tkren@kr.tuwien.ac.at>
2010-07-18 20:12:47 +0000

    Release dlvhex-scriptplugin 1.7.1.

---
+++ tags/Release_1_7_1/examples/example2/int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+print "12345"
+print "X2345"
+print "12X45"
+print "1234X"
+print "XXXXX"
---
+++ tags/Release_1_7_1/include/ScriptPlugin.h
@@ -0,0 +1,39 @@
+/* -*- C++ -*- */
+
+/**
+ * @file   ScriptPlugin.h
+ * @author Anton Korosec
+ * @date   February 2007
+ *
+ * @brief  script-plugin for DLVHEX
+ */
+
+#if !defined(_DLVHEX_SCRIPTPLUGIN_H)
+#define _DLVHEX_SCRIPTPLUGIN_H
+
+#include "ScriptAtom.h"
+#include "ScriptConverter.h"
+#include <dlvhex/PluginInterface.h>
+
+namespace dlvhex {
+  namespace script {
+
+class ScriptPlugin : public PluginInterface {
+public:
+    ScriptPlugin();
+    ~ScriptPlugin();
+    PluginConverter* createConverter();
+    void getAtoms(AtomFunctionMap& m);
+    void setOptions(bool doHelp, std::vector<std::string>& argv,
+                    std::ostream& out);
+
+private:
+    bool activatePlugin;
+    std::string addToPath;
+    ScriptConverter* converter;
+};
+
+  } // namespace script
+} // namespace dlvhex
+
+#endif // _DLVHEX_SCRIPTPLUGIN_H
